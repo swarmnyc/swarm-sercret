f6739c3f51f9fb47326ea54e5489c73897a141ed
Jacek Grębski <jgrebski@gmail.com>
2014-09-28 00:54:01 -0400

    Various bug fixes

--- index.php
+++ index.php
@@ -32,17 +32,79 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['status'])) {
     
 	  <!-- TWITTER PULL SCRIPT GOES HERE TO EDIT WHAT ACCOUNT TWEETS PULL FROM EDIT "pullme.php'-->
 	  <?php include_once('pullme.php') ?>
+	  
+	  <style type="text/css">
+        .stage {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            min-width: 900px;
+            height: 40%;
+            overflow: hidden;
+        }
+        .far-clouds {
+            background: transparent url(img/far-clouds.png) 305px 102px repeat-x; z-index: -1;
+        }
+        .near-clouds {
+            background: transparent url(img/near-clouds.png) 305px 162px repeat-x; z-index: -1;
+        }
+	</style>
+	  
+	<script src="js/jquery-1.6.3.min.js" type="text/javascript"></script>
+    <script src="js/jquery.spritely-0.6.js" type="text/javascript"></script>
+	  
+	   <script type="text/javascript">
+            $(document).ready(function() {
+                $('#far-clouds').pan({fps: 30, speed: 0.7, dir: 'left', depth: 30});
+                $('#near-clouds').pan({fps: 30, speed: 1, dir: 'left', depth: 70});
+                
+                window.actions = {
+                    speedyClouds: function(){
+                        $('#far-clouds').spSpeed(12);
+                        $('#near-clouds').spSpeed(20);
+                    },
+                    runningClouds: function(){
+                        $('#far-clouds').spSpeed(8);
+                        $('#near-clouds').spSpeed(12);
+                    },
+                    walkingClouds: function(){
+                        $('#far-clouds').spSpeed(3);
+                        $('#near-clouds').spSpeed(5);
+                    },
+                    lazyClouds: function(){
+                        $('#far-clouds').spSpeed(0.7);
+                        $('#near-clouds').spSpeed(1);
+                    },
+                    stop: function(){
+                        $('#far-clouds, #near-clouds').spStop();
+                    },
+                    start: function(){
+                        $('#far-clouds, #near-clouds').spStart();
+                    },
+                    toggle: function(){
+                        $('#far-clouds, #near-clouds').spToggle();
+                    },
+                    left: function(){
+                        $('#far-clouds, #near-clouds').spChangeDir('left');                    
+                    },
+                    right: function(){
+                        $('#far-clouds, #near-clouds').spChangeDir('right');                    
+                    }
+                };
+            });    
+    </script>
 		  
   </head>
   
-  <body id="thankPageBody">
-     <div class="container"> 
+<body id="#">
+    <div class="container"> 
       
        <!-- START MAIN CTA AREA -->
 	  <div class="contain-main">
 	  	<div class="logo" id="idxLogo">
 		  	<a href="http://swarmnyc.com"><img src="img/logo.jpg" alt="SWARM NYC" id="SWARMNYC" width="300px;"></a><br>
-		  	<span class="logo-desc pinkish">Tweet anonymously from <a class="whitey" href="http://twitter.com/swarmbot5000" target="_blank">@swarmbot5000</a> to anyone you want to with this fun web app.<br> </span>
+		  	<span class="logo-desc pinkish">Tweet anonymously from <a class="whitey" href="http://twitter.com/SWARMsecret" target="_blank">@SWARMsecret</a> to anyone you want to.<br> </span>
 		</div>
     
 		<div class="wrapitup">
@@ -50,7 +112,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['status'])) {
     
 	  	<!-- BEGIN TWEET FORM -->
   			<div class="clearfix" id="thankPageTweetForm">
-  				<textarea maxlength="140" name="status" rows="3" placeholder="Tweet at anyone from @SwarmBOT5000 i.e.  We @WhiteHouse want free ice cream for everyone!!! via @swarmnyc"></textarea>
+  				<textarea maxlength="140" name="status" rows="3" placeholder="Tweet anonymously at anyone from @SWARMsecret. - i.e. Hey @WhiteHouse, we want free ice cream for everyone!!!" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Tweet anonymously at anyone from @SWARMsecret. - i.e. Hey @WhiteHouse, we want free ice cream for everyone!!!'"></textarea>
   				<button type="submit" class="btn btn-default pull-right righton" id="thankPageTweetSubmit">TWEET</button>	
   			</div>
   
@@ -63,18 +125,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['status'])) {
 			</div>
       </div>
 	  </div>
+	  
+	<div id="far-clouds" class="far-clouds stage"></div>
+    <div id="near-clouds" class="near-clouds stage"></div>
 
     <!-- START FOOTER -->
 	<div class="footer">
-		<div class="swarm">Made by <a class="whitey" href="http://swarmnyc.com" target="_blank">SWARM</a> | Grab the code at <a class="whitey" href="http://github.com/swarmnyc">GitHub</a>. 
+		<div class="swarm">Made by <a class="whitey" href="http://swarmnyc.com" target="_blank">SWARM</a> | See other <a class="whitey" href="http://swarmnyc.com/experiments">Experiments</a> | Or grab the code at <a class="whitey" href="http://github.com/swarmnyc">GitHub</a> | <a class="whitey" href="tos.html" target="_blank">Terms of Service </a>
 		</div>
     </div>
-
     
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <!--  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> -->
     <script src="../../dist/js/bootstrap.min.js"></script>
     <script src="../../assets/js/docs.min.js"></script>   
     
